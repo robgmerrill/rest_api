908d23cff921533afe6e64ffde2015dfed9d6ef1
Rob Merrill <robgmerrill@gmail.com>
2016-02-16 09:14:04 -0800

    working on routes

--- rob_merrill/server.js
+++ rob_merrill/server.js
@@ -5,14 +5,16 @@ const app = module.exports = exports = express();
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/beers_app_dev');
 
-const beersRouter = require(__dirname + '/routes/beers');
-const brewersRouter = require(__dirname + '/routes/brewers');
+//changed from plural to singular (beers/brewers)
+const beerRouter = require(__dirname + '/routes/beers');
+const brewerRouter = require(__dirname + '/routes/brewers');
 const beersDrankRouter = require(__dirname + '/routes/beersDrankRouter');
 const userRouter = require(__dirname + '/routes/userRoutes');
 
-app.use('/api', beersRouter);
-app.use('/api', brewersRouter);
+app.use('/api', beerRouter);
+app.use('/api', brewerRouter);
 app.use('/api', userRouter);
+// app.use('/beersDrank', beersDrankRouter);
 
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('server is up on port: ' + PORT));
