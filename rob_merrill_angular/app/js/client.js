b1c200f542555260af35a2bcf98e0d3711c9b1a0
Rob Merrill <robgmerrill@gmail.com>
2016-02-16 16:44:40 -0800

    angular crud updates

--- rob_merrill_angular/app/js/client.js
+++ rob_merrill_angular/app/js/client.js
@@ -16,19 +16,39 @@ beerApp.controller('beerController', ['$scope', '$http', ($scope, $http) => {
 
     $http.get('http://localhost:3000/api/brewers')
     .then((res) => {
-      constole.log('success!');
+      console.log('success!');
       $scope.brewers = res.data;
     }, (err) => {
       console.log(err);
     });
 
-    
+    $scope.beersDrank = function() {
+      $http.get('http://localhost:3000/beersDrank/howManyBeers')
+        .then((res) => {
+          console.log('brews for everyone!');
+          $scope.beersDrank = res.data;
+        }, (err) => {
+          console.log(err);
+        });
+    };
+
+    $scope.beersDrank();
 
   $scope.createBeer = function(beer) {
     $http.post('http://localhost:3000/api/beers', beer)
       .then((res) => {
         $scope.beers.push(res.data);
         $scope.newBeer = null;
+      }, (err) => {
+        console.log(err)
+      })
+  }
+
+    $scope.createBrewer = function(brewer) {
+    $http.post('http://localhost:3000/api/brewers', brewer)
+      .then((res) => {
+        $scope.brewers.push(res.data);
+        $scope.newBrewer = null;
       }, (err) => {
         console.log(err)
       })
@@ -43,7 +63,16 @@ beerApp.controller('beerController', ['$scope', '$http', ($scope, $http) => {
       })
   }
   
-  scope.updateBeer = function(beer) {
+    $scope.deleteBrewer = function(brewer) {
+    $http.delete('http://localhost:3000/api/brewers/' + brewer._id)
+      .then((res) => {
+        $scope.brewers = $scope.brewers.filter((i) => i !== beer);
+      }, (err) => {
+        console.log(err)
+      })
+  }
+
+  $scope.updateBeer = function(beer) {
     $http.put('http://localhost:3000/api/beers/' + beer._id, beer)
       .then((res) => {
         $scope.beers[$scope.beers.indexOf(beer)] = beer;
@@ -51,6 +80,17 @@ beerApp.controller('beerController', ['$scope', '$http', ($scope, $http) => {
       }, (err) => {
         console.log(err);
         beer.editing = false;
+      });
+  }
+
+   $scope.updateBrewer = function(brewer) {
+    $http.put('http://localhost:3000/api/brewer/' + brewer._id, brewer)
+      .then((res) => {
+        $scope.brewers[$scope.brewer.indexOf(brewer)] = brewer;
+        brewer.editting = false;
+      }, (err) => {
+        console.log(err);
+        brewer.editing = false;
       });
   }
 
